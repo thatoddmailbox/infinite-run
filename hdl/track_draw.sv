50a7359aa54f1b55129d68cdbf32be22ba374c7f
Alex Studer <alex@studer.dev>
2021-11-22 15:34:38 -0500

    hdl/track_draw: display obstacles in different colors

--- hdl/track_draw.sv
+++ hdl/track_draw.sv
@@ -67,8 +67,38 @@ module track_draw(
         end
     endgenerate
 
+    // TODO: is this ok?
+    wire [3:0] obstacle_index = (
+        (obstacle_active[0]) ? 4'd0 :
+        (obstacle_active[1]) ? 4'd1 :
+        (obstacle_active[2]) ? 4'd2 :
+        (obstacle_active[3]) ? 4'd3 :
+        (obstacle_active[4]) ? 4'd4 :
+        (obstacle_active[5]) ? 4'd5 :
+        (obstacle_active[6]) ? 4'd6 :
+        (obstacle_active[7]) ? 4'd7 :
+        (obstacle_active[8]) ? 4'd8 :
+        (obstacle_active[9]) ? 4'd9 : 4'hF
+    );
+    wire [11:0] obstacle_color = (
+        (obstacle_active[0]) ? 12'hF00 :
+        (obstacle_active[1]) ? 12'h0F0 :
+        (obstacle_active[2]) ? 12'hFF0 :
+        (obstacle_active[3]) ? 12'hFFF :
+        (obstacle_active[4]) ? 12'hFFF :
+        (obstacle_active[5]) ? 12'hFFF :
+        (obstacle_active[6]) ? 12'hFFF :
+        (obstacle_active[7]) ? 12'hFFF :
+        (obstacle_active[8]) ? 12'hFFF :
+        (obstacle_active[9]) ? 12'hFFF : 12'hFFF
+    );
+
     always_ff @(posedge system_clock_in) begin
-        rgb <= (!in_lane_margin && (obstacle_active[0] || obstacle_active[1] || obstacle_active[2]) ? 12'hFFF :
+        rgb <= ((!in_lane_margin && obstacle_index != 4'hF) ?
+            // obstacle
+            obstacle_color :
+
+            // lane background
             (current_lane << 12'd2)
         );
     end
