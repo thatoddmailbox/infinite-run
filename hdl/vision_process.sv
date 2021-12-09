fe3746ac09ba72fe725d0da5728416463d14eae6
Alex Studer <alex@studer.dev>
2021-12-09 11:04:36 -0500

    hdl/vision_process: handle being offscreen

--- hdl/vision_process.sv
+++ hdl/vision_process.sv
@@ -20,6 +20,7 @@ module vision_process(
     wire frame_start = (frame_x_count == 10'd0 && frame_y_count == 9'd0);
 
     // nine quadrants of each frame
+    // (i realize that quadrants is technically the wrong term since there's more than four of them)
     // +-----------------+
     // |  0  |  1  |  2  |
     // |  3  |  4  |  5  |
@@ -42,8 +43,6 @@ module vision_process(
                 2'd1 : 2'd2
             )
     );
-    // TODO: this is weird, can we do better? does it synthesize well?
-    // (maybe do addition instead?
     wire [3:0] quadrant =
         (quadrant_col == 2'd0 && quadrant_row == 2'd0) ? 4'd0 :
         (quadrant_col == 2'd1 && quadrant_row == 2'd0) ? 4'd1 :
@@ -58,6 +57,7 @@ module vision_process(
     wire [2:0] lane_0_quadrants = quadrants[0] + quadrants[3] + quadrants[6];
     wire [2:0] lane_1_quadrants = quadrants[1] + quadrants[4] + quadrants[7];
     wire [2:0] lane_2_quadrants = quadrants[2] + quadrants[5] + quadrants[8];
+    wire [3:0] total_quadrants = lane_0_quadrants + lane_1_quadrants + lane_2_quadrants;
 
     always_ff @(posedge pixel_clock_in) begin
         if (frame_start && !pixel_valid) begin
@@ -66,13 +66,21 @@ module vision_process(
             // (that way we still process the first pixel correctly)
 
             // set lane and jump
-            // TODO: this is kinda weird
-            lane <= (
-                (lane_2_quadrants >= lane_0_quadrants && lane_2_quadrants >= lane_1_quadrants) ? 2'd2 :
-                (lane_1_quadrants >= lane_0_quadrants && lane_1_quadrants >= lane_2_quadrants) ? 2'd1 :
-                (lane_0_quadrants >= lane_1_quadrants && lane_0_quadrants >= lane_2_quadrants) ? 2'd0 : 2'd3
-            );
-            jump <= ~(quadrants[6] || quadrants[7] || quadrants[8]);
+            if (total_quadrants < 2) begin
+                // if there's only zero or one active quadrants, we don't really have enough data
+                // (user's probably offscreen?)
+                // so, put them in the non-existent lane 3, and force them to be on the ground.
+                // that way, the game doesn't start reacting to random inputs by itself
+                lane <= 2'd3;
+                jump <= 1'd0;
+            end else begin
+                lane <= (
+                    (lane_2_quadrants >= lane_0_quadrants && lane_2_quadrants >= lane_1_quadrants) ? 2'd2 :
+                    (lane_1_quadrants >= lane_0_quadrants && lane_1_quadrants >= lane_2_quadrants) ? 2'd1 :
+                    (lane_0_quadrants >= lane_1_quadrants && lane_0_quadrants >= lane_2_quadrants) ? 2'd0 : 2'd3
+                );
+                jump <= ~(quadrants[6] || quadrants[7] || quadrants[8]);
+            end
 
             // set data valid
             data_valid <= 1'b1;
